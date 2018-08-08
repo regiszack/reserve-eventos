f53b2d38d9701f5b3491ebbff979ad83ab25e19d
Regis Zack <regiszacharias@hotmail.com>
2018-08-08 15:19:40 -0300

    Add files via upload

---
+++ reserve-app/src/services/reserve/RestClientsManager.js
@@ -0,0 +1,45 @@
+import axios from 'axios'
+import { toast } from 'react-toastify'
+import { Cartao, Evento, Pedido, Usuario } from './clients'
+
+export default class RestClientsManager {
+
+  constructor() {
+    
+    this.config = {
+      "baseURL": 'http://localhost:58952/api',
+    }
+
+    this.axios = axios.create(this.config)
+
+    this.axios.interceptors.response.use(
+      resposta => {       
+        return resposta
+      },
+      erro => {
+        return Promise.reject(erro)
+        }
+    )
+
+    this.cartaoClient = new Cartao(this.axios)
+    this.eventoClient = new Evento(this.axios)
+    this.pedidoClient = new Pedido(this.axios)
+    this.usuarioClient = new Usuario(this.axios)
+  }
+
+  get cartao() {
+    return this.cartaoClient
+  }
+
+  get evento() {
+    return this.eventoClient
+  }
+
+  get pedido() {
+    return this.pedidoClient
+  }
+
+  get usuario() {
+    return this.usuarioClient
+  }
+}
