40610af246faf1a87a68d0d492a4dbc34cd7185d
REGIS-PC\regis <regiszacharias@hotmail.com>
2018-08-09 21:16:25 -0300

    Remoção de Método não utilizado

--- reserve-service/WebApplication1/Helpers/CriptografarDados.cs
+++ reserve-service/WebApplication1/Helpers/CriptografarDados.cs
@@ -81,21 +81,5 @@ namespace WebApi.Helpers
                 }
             }
         }
-
-        private static string GerarKey()
-        {
-            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-            var stringChars = new char[6];
-            var random = new Random();
-
-            for (int i = 0; i < stringChars.Length; i++)
-            {
-                stringChars[i] = chars[random.Next(chars.Length)];
-            }
-
-            var finalString = new String(stringChars);
-
-            return finalString;
-        }
     }
 }
